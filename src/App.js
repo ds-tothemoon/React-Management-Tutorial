0da83dff27d22af8bb83075da4c8c117f2c2e87e
mdjuniooor <mmdsds@u.sogang.ac.kr>
2020-04-09 18:50:34 +0900

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="logo"/>
+      <h2>Let's develop management system!</h2>
+    </div>
+  );
+}
+
+export default App;
